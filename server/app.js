82cc5777bfa774cf9eee3605839d1c0b7a04dc0b
cheonHwi <hoba0409@naver.com>
2023-02-26 01:52:13 +0900

    setting docker files

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
-const express = require("express");
-const app = express();
+"use strict";
 
+const express = require("express");
+const morgan = require("morgan");
+
+// 상수
 const PORT = 3000;
 
+// 앱
+const app = express();
+
+//로깅
+app.use(morgan("common"));
+
 app.get("/", (req, res) => {
-  res.send("asd");
+  res.send("Hello World");
 });
 
-app.listen(PORT, () => {
-  console.log(`server open at http://localhost:3000`);
-});
+app.listen(PORT, console.log(`Running on http://localhost:${PORT}`));
