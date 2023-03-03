afa607cd52f711173e8a9941ec06fd7ce81418ed
cheonHwi <hoba0409@naver.com>
2023-03-03 23:06:53 +0900

    오늘 작업 마무리

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 // 상수
 const PORT = 3000;
@@ -11,9 +12,10 @@ const app = express();
 
 //로깅
 app.use(morgan("common"));
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("test data");
 });
 
 app.listen(PORT, console.log(`Running on http://localhost:${PORT}`));
