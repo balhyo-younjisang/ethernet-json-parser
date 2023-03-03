f0efba6129bc90658cb287373710d0d4dfd57975
cheonHwi <hoba0409@naver.com>
2023-03-03 23:07:10 +0900

    오늘 작업 마무리

--- frontend/src/container/indexContainer/index.jsx
+++ frontend/src/container/indexContainer/index.jsx
@@ -1,7 +1,16 @@
 import { Header } from "./items/header";
 import { Data } from "./items/data";
+import { useEffect } from "react";
+import axios from "axios";
 
 export const MainContainer = () => {
+  useEffect(() => {
+    async function fetchData() {
+      const data = await axios.get("http://localhost:3000/");
+      console.log(data);
+    }
+    fetchData();
+  }, []);
   return (
     <>
       <Header></Header>
