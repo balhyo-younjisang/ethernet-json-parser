0e62041eb783d514a297b06e458b1e160f11534a
cheonHwi <hoba0409@naver.com>
2023-03-05 22:26:55 +0900

    create : periodic execution function

--- frontend/src/container/indexContainer/index.jsx
+++ frontend/src/container/indexContainer/index.jsx
@@ -1,10 +1,18 @@
 import { Header } from "./items/header";
 import { Data } from "./items/data";
-import { useEffect } from "react";
-import fetchData from "../../api/fetchData";
+import { useEffect, useState } from "react";
 import axios from "axios";
+// import fetchData from "../../api/fetchData";
 
 export const MainContainer = () => {
+  const [data, setData] = useState();
+  async function fetchData() {
+    const { data } = await axios.get("http://localhost:3000/");
+    console.log(data);
+    setData(data);
+    setTimeout(fetchData, 1000); // 1초 후에 fetchData 함수를 다시 호출합니다.
+  }
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -12,8 +20,8 @@ export const MainContainer = () => {
   return (
     <>
       <Header></Header>
-      {[...Array(parseInt(5))].map((n, index) => {
-        return <Data key={index} />;
+      {[...Array(parseInt(1))].map((n, index) => {
+        return <Data key={index} data={data} />;
       })}
     </>
   );
