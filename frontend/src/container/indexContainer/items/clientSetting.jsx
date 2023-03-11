0889927ca2fcee9b709050f9e16a19bc8eddefe8
balhyo-jisang <yjs12180825@gmail.com>
2023-03-12 04:34:35 +0900

    client setting ui

--- frontend/src/container/indexContainer/items/clientSetting.jsx
+++ frontend/src/container/indexContainer/items/clientSetting.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import close from "/rectangle-xmark-regular.svg";
+import upload from "/upload-svgrepo-com.svg";
+import increase from "/Increase.svg";
+import decrease from "/decrease.svg";
 
 export const ClientSetting = ({ setModalOpen }) => {
   const hideSetModal = () => {
     setModalOpen(false);
   };
+
+  const settingLabels = [
+    "Heater Operation value",
+    "Cooler Operation value",
+    "Dehumidification value",
+  ];
 
   return (
     <>
@@ -16,10 +25,47 @@ export const ClientSetting = ({ setModalOpen }) => {
               <img src={close} width="25"></img>
             </Button>
             <SettingContainer>
-              <CommandSetting>a</CommandSetting>
-              <ButtonSetting>b</ButtonSetting>
-              <Button type="submit">Save</Button>
+              <CommandSetting>
+                <Wrap>
+                  <div>
+                    <p>Name setting</p>
+                  </div>
+                  <div>
+                    <input type="text" />
+                    <img src={upload} width="20" height="20" />
+                  </div>
+                </Wrap>
+                {settingLabels.map((label, index) => (
+                  <Wrap key={index}>
+                    <div>
+                      <p>{label}</p>
+                    </div>
+                    <div>
+                      <img src={increase} width="20" height="20" />
+                      <input type="text" />
+                      <img src={decrease} width="20" height="20" />
+                    </div>
+                  </Wrap>
+                ))}
+              </CommandSetting>
+              <ButtonSetting>
+                <ButtonWrap>
+                  <RebootBtn>REBOOT SYSTEM</RebootBtn>
+                </ButtonWrap>
+                <ManualControlWrap>
+                  <label>Manual Control</label>
+                  <SwitchWrap>
+                    <label>Heater</label>
+                    <Switch alt="switch" />
+                  </SwitchWrap>
+                  <SwitchWrap>
+                    <label>Cooler</label>
+                    <Switch alt="switch" />
+                  </SwitchWrap>
+                </ManualControlWrap>
+              </ButtonSetting>
             </SettingContainer>
+            <SaveButton type="submit">Save</SaveButton>
           </Modal>
         </Window>
       </Container>
@@ -54,7 +100,9 @@ const Modal = styled.div`
 `;
 
 const SettingContainer = styled.div`
+  height: 30vh;
   display: grid;
+  grid-template-columns: 2fr 1fr;
 `;
 
 const Button = styled.button`
@@ -62,10 +110,56 @@ const Button = styled.button`
   border: 0px;
 `;
 
+const SaveButton = styled.button`
+  background-color: #ffffff;
+  border: 0px;
+  margin-left: 45%;
+`;
+
 const CommandSetting = styled.div`
-  grid-template-columns: 2fr;
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
 `;
 
 const ButtonSetting = styled.div`
-  grid-template-columns: 1fr;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-item: center;
+  justify-content: space-around;
+`;
+
+const Switch = styled.img`
+  content: url(${(props) =>
+    props.clicked ? "togle_on.svg" : "togle_off.svg"});
+  user-select: none;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  width: 5vw;
+`;
+
+const SwitchWrap = styled.div`
+  width: 7vw;
+`;
+
+const ManualControlWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
+
+const ButtonWrap = styled.div`
+  text-align: center;
+`;
+
+const RebootBtn = styled.button`
+  border-radius: 10px;
+  border: 1px;
+  padding: 10px;
 `;
