5f9abd93f234ee9f6aac0b4734236892049cf5f1
cheonHwi <hoba0409@naver.com>
2023-04-27 00:31:01 +0900

    add : refresh function in icon

--- frontend/src/container/indexContainer/items/titlebar.jsx
+++ frontend/src/container/indexContainer/items/titlebar.jsx
@@ -24,7 +24,7 @@ export const Titlebar = (props) => {
   return (
     <Container>
       {/* <Icon onClick={showModal}> */}
-      <Icon>
+      <Icon onClick={() => location.reload()}>
         <img src="logo.png" alt="logo" width="60"></img>
       </Icon>
 
